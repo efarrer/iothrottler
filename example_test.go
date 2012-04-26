fa823a7dd203eedae80441eb0aca4baedd9c9f90
Evan Farrer <Evan.R.Farrer@raytheon.com>
2012-04-25 23:56:58 -0600

    Push examples into their own file.
    Update documentation with new and improved examples

--- example_test.go
+++ example_test.go
@@ -1,26 +1,53 @@
-iothrottler
-===========
+package iothrottler
 
-A Go package for throttling application IO (such as bandwidth).
-
-Installation
-------------
-
-If you have the Go Language installed type
-`go get "github.com/efarrer/iothrottler"`
-
-Example
--------
 import (
-. "github.com/efarrer/iothrottler"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
 )
+
+/*
+ * Basic usage of a IOThrottlerPool to throttle reading from a file 
+ */
+func ExampleIOThrottlerPool() {
+	// Construct a bandwidth throttling pool that's limited to 100 bytes per second
+	pool := NewIOThrottlerPool(BytesPerSecond * 100)
+	defer pool.ReleasePool()
+
+	file, err := os.Open("/dev/zero")
+	if err != nil {
+		// handle error
+		return
+	}
+	defer file.Close()
+
+	throttledFile, err := pool.AddReader(file)
+	if err != nil {
+		// handle error
+		return
+	}
+
+	var zeros bytes.Buffer
+
+	_, err = io.CopyN(&zeros, throttledFile, 200)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println("Done")
+	// Output: Done
+}
 
 /*
  * Throttle web requests using an IOThrottlerPool
  */
 func ExampleIOThrottlerPool_AddConn() {
 	// Construct a bandwidth throttling pool that's limited to 30 kilobits per
-    // second
+	// second
 	pool := NewIOThrottlerPool(Kbps * 30)
 	defer pool.ReleasePool()
 
