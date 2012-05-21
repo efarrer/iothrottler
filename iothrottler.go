6c9286c66909728e605b229cfef04a680f5836b2
Evan Farrer <Evan.R.Farrer@raytheon.com>
2012-05-21 09:31:41 -0600

    Provide an explicit BSD 2 clause license.
    This should make the lawyers of the world happy and make it clear to developers
    that they can use this package freely.

--- iothrottler.go
+++ iothrottler.go
@@ -1,3 +1,11 @@
+// Copyright 2012 Evan Farrer. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+	Package iothrottler implements application IO throttling.
+*/
+
 package iothrottler
 
 import (
